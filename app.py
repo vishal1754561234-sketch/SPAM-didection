aa503a82e5f0cae2cd58592f79f2c547a6a4a973
vishal1754561234-sketch <vishal1754561234@gmail.com>
2026-01-08 16:17:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from nltk.stem import WordNetLemmatizer
 
 # Download necessary NLTK data
 nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 
